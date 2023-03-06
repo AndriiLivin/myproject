14dd5e3d51ab14285d182e1f55466c47c8a681b6
Andrii Livinskyi <landry2022@ukr.net>
2023-03-06 23:51:19 +0200

    my first projects commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { type } from "os";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+import { Footer } from "./Footer";
+
+const user = { userName: "Andrii" };
+
+const App = () => (
+  <div className="App">
+    <h1> Привет мой свет {user.userName} !</h1>
+    <Footer copyright=" C" />
+  </div>
+);
 
 export default App;
